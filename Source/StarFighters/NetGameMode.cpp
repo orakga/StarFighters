a8fc0a3865a2c0fbe5e07e9a1b0b3d8d6b71e3a6
Bochan Kim <orakga@gmailcom>
2025-06-22 11:00:36 +0900

    Session 115: Quality of Life Tips #1

--- Source/StarFighters/NetGameMode.cpp
+++ Source/StarFighters/NetGameMode.cpp
@@ -9,15 +9,13 @@ void ANetGameMode::BeginPlay()
 	Super::BeginPlay();
 	
 
-	UE_LOG(LogTemp, Warning, TEXT("HELLO WORLD!!"));
-
 	if (FParse::Value(FCommandLine::Get(), TEXT("servername="), serverName))
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Server Name is: %s"), *serverName);
+		UE_LOG(LogTemp, Warning, TEXT("ANetGameMode::BeginPlay() Server Name is: %s"), *serverName);
 	}
 	else
 	{
-		UE_LOG(LogTemp, Error, TEXT("Server Name NOT PROVIDED: %s"), *serverName);
+		UE_LOG(LogTemp, Error, TEXT("ANetGameMode::BeginPlay() ERROR | Server Name NOT PROVIDED: %s"), *serverName);
 	}
 	
 }
